b8215235fda2004be62a82711d98d50aff14b0af
Sahil Sharma <sahilsharma011@gmail.com>
2016-01-09 21:37:06 +0530

    Test Changes for mobile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,43 +3,85 @@
     <head>
         <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="{{asset('css/fullcalendar.min.css')}}" rel="stylesheet" type="text/css">
+        <script src="{{asset('js/jquery.min.js')}}"></script>
+        <script src="{{asset('js/jquery.ui.touch-punch.min.js')}}"></script>
+        <script src="{{asset('js/moment.js')}}"></script>
+        <script src="{{asset('js/fullcalendar.min.js')}}"></script>
+        <script>
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+            $(document).ready(function() {
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+                $('#calendar').fullCalendar({
+                    header: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'month,agendaWeek,agendaDay'
+                    },
+                    defaultDate: '2014-06-12',
+                    dayClick: function(date, jsEvent, view) {
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+                        alert('Clicked on: ' + date.format());
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+                        alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
 
-            .title {
-                font-size: 96px;
-            }
-        </style>
+                        alert('Current view: ' + view.name);
+
+                        // change the day's background color just for fun
+                        $(this).css('background-color', 'red');
+
+                    },
+                    defaultView: 'month',
+                    editable: true,
+                    events: [
+                        {
+                            title: 'All Day Event',
+                            start: '2014-06-01'
+                        },
+                        {
+                            title: 'Long Event',
+                            start: '2014-06-07',
+                            end: '2014-06-10'
+                        },
+                        {
+                            id: 999,
+                            title: 'Repeating Event',
+                            start: '2014-06-09T16:00:00'
+                        },
+                        {
+                            id: 999,
+                            title: 'Repeating Event',
+                            start: '2014-06-16T16:00:00'
+                        },
+                        {
+                            title: 'Meeting',
+                            start: '2014-06-12T10:30:00',
+                            end: '2014-06-12T12:30:00'
+                        },
+                        {
+                            title: 'Lunch',
+                            start: '2014-06-12T12:00:00'
+                        },
+                        {
+                            title: 'Birthday Party',
+                            start: '2014-06-13T07:00:00'
+                        },
+                        {
+                            title: 'Click for Google',
+                            url: 'http://google.com/',
+                            start: '2014-06-28'
+                        }
+                    ]
+
+                });
+
+            });
+
+        </script>
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
+    <div id="calendar"></div>
+
+
     </body>
 </html>
