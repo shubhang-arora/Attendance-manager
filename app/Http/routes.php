bbee93127d8ef32ef0a1ec82026cd34d97c058bd
shubhang-arora <sa287@snu.edu.in>
2016-04-11 15:41:34 +0530

    Added tables

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,3 +29,9 @@ Route::get('/', function () {
 Route::group(['middleware' => ['web']], function () {
     //
 });
+
+Route::get('/auth/logout', 'AuthNewController@logout');
+Route::get('/auth/google','AuthNewController@handleProviderCallback');
+Route::get('/auth/verify','AuthNewController@verifyUser');
+Route::post('/auth/verify','AuthNewController@verifyUserPost');
+Route::get('/auth/login', 'AuthNewController@redirectToProvider');
---
+++ database/migrations/2016_04_11_153602_create_faculty_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFacultyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('faculties', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->integer('course_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('course_id')
+                ->references('id')
+                ->on('courses')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('faculties');
+    }
+}
