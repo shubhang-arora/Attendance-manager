70aa5143847586a54d572c3aa365288240f3eea8
shubhang-arora <sa287@snu.edu.in>
2016-04-11 15:51:31 +0530

    Timing table

---
+++ database/migrations/2016_04_11_154855_create_timings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timings', function (Blueprint $table) {
+            $table->integer('course_id')->unsigned();
+            $table->timestamp('start_time');
+            $table->timestamp('end_time');
+            $table->timestamps();
+
+
+            $table->foreign('course_id')
+                ->references('id')
+                ->on('courses')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('timings');
+    }
+}
